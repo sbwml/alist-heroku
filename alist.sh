ec3f392cd7a603bdab0910f6962e184d4a6c902d
sbwml <984419930@qq.com>
2021-11-05 21:49:36 +0800

    创建配置文件

--- alist.sh
+++ alist.sh
@@ -1,5 +1,22 @@
 # 创建配置文件目录
 mkdir -p /alist/config/
 
+cat >/alist/config/config.json <<EOF
+{
+  "address": "0.0.0.0",
+  "port": $PORT,
+  "database": {
+    "type": "sqlite3",
+    "user": "",
+    "password": "",
+    "host": "",
+    "port": 0,
+    "name": "",
+    "table_prefix": "x_",
+    "db_file": "/alist/config/data.db"
+  }
+}
+EOF
+
 cd /alist
 ./alist -conf config/config.json
